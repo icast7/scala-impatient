e885abce5d1bc6567326f2896b94a435c54eaab2
icaste <ignacio.castillejos@nike.com>
2016-06-16 19:10:07 -0700

    Added 6.5

--- src/main/scala/_6_5_scalaApp.scala
+++ src/main/scala/_6_5_scalaApp.scala
@@ -5,10 +5,20 @@
   */
 object scalaApp extends App {
   var msg : String = ""
+  //1
+  msg = ""
   for (i <- (args.length-1) to 0 by -1) {
     msg += args(i) + " "
   }
   println(msg)
+  //2
+  msg = ""
+  for (i <- args.reverse) {
+    msg += i + " "
+  }
+  println(msg)
+  //3
+  print(args.reverse.mkString(" "))
 }
-//scalac Users/icaste/scala-impatient/src/main/scala/_6_5_scalaApp.scala
+//scalac /Users/icaste/scala-impatient/src/main/scala/_6_5_scalaApp.scala
 //scala -Dscala.time scalaApp test1 test2 test3 test4
